ec46cbb8e675bf39ff9ba53dcfe525d2f10ec544
YuvrajPraveenSingh <yuv00191@outlook.com>
2024-03-27 21:54:48 +0530

    Add validation for category, ingredients, instructions, allergens, and chronicDiseases fields

--- backend/src/controllers/recipe/createRecipe.js
+++ backend/src/controllers/recipe/createRecipe.js
@@ -3,15 +3,45 @@ import ApiError from "../../utils/ApiError.js";
 import {Recipe} from "../../models/recipe.models.js"
 import ApiResponse from "../../utils/ApiResponse.js";
 
+
+const validCategories = ['General', 'Vegetarian', 'Vegan', 'Gluten-free', 'Dairy-free', 'Nut-free'];
+
+
 const createRecipe =  AsyncHandaler(async (req , res) =>{
-   const {title, description} = req.body;
-   if( [title, description,].some((field) => field === undefined || field?.trim() === "")){
-      throw new ApiError(400 ,"All fields are required");
-    } 
+   const {title, description , category, ingredients, instructions, allergens =[], chronicDiseases =[]} = req.body;
+   if( [title, description].some((field) => field === undefined || field?.trim() === "")){
+      throw new ApiError(400 ,"title & description fields are required");
+   } 
+
+   if([category, ingredients, instructions].some((field) => field === undefined)){
+      throw new ApiError(400 ,"category, ingredients, instructions fields are required");
+   }
+
+   if (allergens && (!Array.isArray(allergens) || allergens?.some(item => typeof item !== 'string'))) {
+      throw new ApiError(400, "Allergens must be an array of strings");
+  }
+
+  if (chronicDiseases && (!Array.isArray(chronicDiseases) || chronicDiseases?.some(item => typeof item !== 'string'))) {
+   throw new ApiError(400, "chronicDiseases must be an array of strings");
+}
+
+   if (!Array.isArray(ingredients) || ingredients.length === 0) {
+      throw new ApiError(400 ,"Ingredients must be a non-empty array");
+   }
+
+   if (!Array.isArray(instructions) || instructions.length === 0) {
+      throw new ApiError(400 ,"Instructions must be a non-empty array");
+   }
+
 
    const recipe = await Recipe.create({
       title,
       description,
+      category,
+      ingredients,
+      instructions,
+      allergens,
+      chronicDiseases
    });
 
    res.status(201).json(
